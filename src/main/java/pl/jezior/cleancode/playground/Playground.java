6b0f1e3be54a5caf37e04f2a4795c986918dc90a
Maciej Jezierski <maciej.jezierski@sabre.com>
2018-06-24 15:43:01 -0500

    q&d

--- src/main/java/pl/jezior/cleancode/playground/Playground.java
+++ src/main/java/pl/jezior/cleancode/playground/Playground.java
@@ -1,7 +1,24 @@
 package pl.jezior.cleancode.playground;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
 public class Playground {
     public String hello() {
         return "World!";
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(Files.readAllLines(Paths.get(Playground.class.getClassLoader().getResource("sherlock.txt").getPath()))
+                .stream()
+                .flatMap(l -> Arrays.stream(l.split("")).filter(s -> s.length() > 0).map(s->s.charAt(0)))
+                .filter(Playground::isUpper)
+                .count());
+    }
+
+    private static boolean isUpper(char c) {
+        return c >= 'A' && c <= 'Z';
     }
 }
